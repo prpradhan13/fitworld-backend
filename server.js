9e7e08cc20b0212385762c2662f0ba1ded536a00
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2024-04-23 11:20:47 +0530

    origin set

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import authRoute from './routes/authRoute.js';
 import categoryRoute from './routes/categoryRoute.js';
 import fileRoute from './routes/fileRoute.js';
 import dietRoute from './routes/dietRoute.js';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
 
 // config env
 dotenv.config({path: "./.env"});
@@ -15,26 +13,20 @@ dotenv.config({path: "./.env"});
 // Database configuration
 connectDB();
 
-//esmodule fix
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-// app.use(express.static(path.join(__dirname, './client/dist')))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoute);
 app.use('/api/category', categoryRoute);
 app.use('/api/file', fileRoute);
 app.use('/api/diet', dietRoute);
-
-// app.use('*', function(req, res){
-//     res.sendFile(path.join(__dirname, './client/dist/index.html'))
-// })
 
 const PORT = process.env.PORT || 8080
 
